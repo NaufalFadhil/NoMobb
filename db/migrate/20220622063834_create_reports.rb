df28be7d2e819008b6eec2cb4d43fb5f3d335ece
Naufal Fadhil Athallah <naufalfadhilathallah@gmail.com>
2022-06-22 13:48:33 +0700

    feat(report): change structure of reports table

--- db/migrate/20220622063834_create_reports.rb
+++ db/migrate/20220622063834_create_reports.rb
@@ -1,16 +1,16 @@
 class CreateReports < ActiveRecord::Migration[7.0]
   def change
     create_table :reports do |t|
-      t.string :user_id
+      t.integer :user_id
       t.string :perpetrator
-      t.string :reason
+      t.text :reason
       t.string :proof
       t.string :witness
       t.string :incident_location
-      t.string :incident_date
+      t.timestamp :incident_date
       t.string :status
-      t.string :is_valid
-      t.string :deleted_at
+      t.boolean :is_valid
+      t.timestamp :deleted_at
 
       t.timestamps
     end
