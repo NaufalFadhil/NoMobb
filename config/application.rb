7f289c897bc33d0dfcf4d630a15dbc5f8381cc89
Naufal Fadhil Athallah <naufalfadhilathallah@gmail.com>
2022-06-23 13:52:13 +0700

    refactor(config): change timezone to Jakarta

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module NoMobb
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.time_zone = "Jakarta"
 
     # Configuration for the application, engines, and railties goes here.
     #
