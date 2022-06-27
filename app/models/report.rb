afea6b0cfa615c2402f1beed7edac9ae37fec9de
Naufal Fadhil Athallah <naufalfadhilathallah@gmail.com>
2022-06-27 18:15:57 +0700

    test(report): create test for input at field report

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,2 +1,7 @@
 class Report < ApplicationRecord
+    validates :perpetrator, presence: true
+    validates :reason, presence: true
+    validates :proof, presence: true
+    validates :witness, presence: true
+    validates :incident_location, presence: true
 end
--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -1,5 +1,88 @@
 require 'rails_helper'
 
 RSpec.describe Report, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Field Report Test" do
+    it 'is valid with full input field' do
+      report = Report.new(
+        user_id: 1,
+        perpetrator: 'Naufal Fadhil Athallah',
+        reason: 'Memukul saya',
+        proof: 'https:drive.google.com',
+        witness: 'Nurma',
+        incident_location: 'Sekolah saya',
+        status: 'PENDING',
+        is_valid: false,
+        deleted_at: nil,
+        created_at: 'Sun, 26 Jun 2022 20:43:18.365332000 WIB +07:00',
+        updated_at: 'Sun, 26 Jun 2022 20:43:18.365332000 WIB +07:00',
+      )
+
+      expect(report).to be_valid
+    end
+
+    it 'is invalid without perpetrator field' do
+      report = Report.new(
+        perpetrator: nil,
+        reason: nil,
+        proof: nil,
+        witness: nil,
+        incident_location: nil,
+      )
+  
+      report.valid?
+      expect(report.errors[:perpetrator]).to include("can't be blank")
+    end
+
+    it 'is invalid without reason field' do
+      report = Report.new(
+        perpetrator: nil,
+        reason: nil,
+        proof: nil,
+        witness: nil,
+        incident_location: nil,
+      )
+  
+      report.valid?
+      expect(report.errors[:reason]).to include("can't be blank")
+    end
+
+    it 'is invalid without proof field' do
+      report = Report.new(
+        perpetrator: nil,
+        reason: nil,
+        proof: nil,
+        witness: nil,
+        incident_location: nil,
+      )
+  
+      report.valid?
+      expect(report.errors[:proof]).to include("can't be blank")
+    end
+
+    it 'is invalid without witness field' do
+      report = Report.new(
+        perpetrator: nil,
+        reason: nil,
+        proof: nil,
+        witness: nil,
+        incident_location: nil,
+      )
+  
+      report.valid?
+      expect(report.errors[:witness]).to include("can't be blank")
+    end
+
+    it 'is invalid without incident location field' do
+      report = Report.new(
+        perpetrator: nil,
+        reason: nil,
+        proof: nil,
+        witness: nil,
+        incident_location: nil,
+      )
+  
+      report.valid?
+      expect(report.errors[:incident_location]).to include("can't be blank")
+    end
+  end
 end
