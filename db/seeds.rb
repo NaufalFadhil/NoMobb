68ab418fae64b2c1f114d67c7364ebdb63c7c8ca
Naufal Fadhil Athallah <naufalfadhilathallah@gmail.com>
2022-06-29 20:50:39 +0700

    feat: add seeds for user dummy

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+users = User.create([
+    { 
+        name: "Naufal Fadhil Athallah",
+        email: "NaufalFadhil@google.com",
+        phone: "08123456789",
+        password: "asdfghjkl",
+        is_verified: true,
+        role: "ADMIN"
+    }
+]);
