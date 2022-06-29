49d53d9d22617d069cd25ffd639ee0ea4739ab05
Naufal Fadhil Athallah <naufalfadhilathallah@gmail.com>
2022-06-29 22:59:51 +0700

    refactor: refactor routes to be simplify

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
-    get 'users', to: 'users#index'
-    get 'users/:id', to: 'users#show'
-    post 'users', to: 'users#create'
-    get 'users/update'
-    get 'users/destroy'
+    resources :users, only: [:index, :show, :create, :destroy]
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
