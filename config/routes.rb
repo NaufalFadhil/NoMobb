f6e966575d6aeb474cf34c9ea2845b43cdde8afd
Naufal Fadhil Athallah <naufalfadhilathallah@gmail.com>
2022-06-29 20:10:34 +0700

    feat: generate users controller api

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  namespace :api do
+    get 'users/index'
+    get 'users/show'
+    get 'users/create'
+    get 'users/update'
+    get 'users/destroy'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
