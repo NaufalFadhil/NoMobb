c90db178ebf974018bcb529037ed3106230f99bb
Naufal Fadhil Athallah <naufalfadhilathallah@gmail.com>
2022-06-30 12:23:55 +0700

    test: add unit test for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    validates :name, presence: true
+    validates :email, presence: true
+    validates :phone, presence: true
+    validates :password, presence: true
+    validates :is_verified, presence: true
+    validates :role, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,100 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with all field' do
+    user = User.new(\
+        name: "Naufal Fadhil Athallah",
+        email: "NaufalFadhil@google.com",
+        phone: "08123456789",
+        password: "adfghjklmnbvcxz",
+        is_verified: true,
+        role: "ADMIN"
+    )
+
+    expect(user).to be_valid
+  end
+
+  it 'is invalid without a name' do
+    user = User.new(
+      name: nil,
+      email: nil,
+      phone: nil,
+      password: nil,
+      is_verified: nil,
+      role: nil
+    )
+
+    user.valid?
+    expect(user.errors[:name]).to include("can't be blank")
+  end
+
+  it 'is invalid without a email' do
+    user = User.new(
+      name: nil,
+      email: nil,
+      phone: nil,
+      password: nil,
+      is_verified: nil,
+      role: nil
+    )
+
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
+  end
+
+  it 'is invalid without a phone' do
+    user = User.new(
+      name: nil,
+      email: nil,
+      phone: nil,
+      password: nil,
+      is_verified: nil,
+      role: nil
+    )
+
+    user.valid?
+    expect(user.errors[:phone]).to include("can't be blank")
+  end
+
+  it 'is invalid without a password' do
+    user = User.new(
+      name: nil,
+      email: nil,
+      phone: nil,
+      password: nil,
+      is_verified: nil,
+      role: nil
+    )
+
+    user.valid?
+    expect(user.errors[:password]).to include("can't be blank")
+  end
+
+  it 'is invalid without a verified' do
+    user = User.new(
+      name: nil,
+      email: nil,
+      phone: nil,
+      password: nil,
+      is_verified: nil,
+      role: nil
+    )
+
+    user.valid?
+    expect(user.errors[:is_verified]).to include("can't be blank")
+  end
+
+  it 'is invalid without a role' do
+    user = User.new(
+      name: nil,
+      email: nil,
+      phone: nil,
+      password: nil,
+      is_verified: nil,
+      role: nil
+    )
+
+    user.valid?
+    expect(user.errors[:role]).to include("can't be blank")
+  end
 end
