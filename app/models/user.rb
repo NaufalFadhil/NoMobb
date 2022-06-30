2846cf183703c40caa32c0a73f4ccd22613f7470
Naufal Fadhil Athallah <naufalfadhilathallah@gmail.com>
2022-06-30 15:39:08 +0700

    feat: handling if input duplicate number in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    validates :email, :phone, presence: true, uniqueness: true
     validates :name, :email, :phone, :password, :role, presence: true
     validates :is_verified, inclusion: { in: [true, false] }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -97,4 +97,50 @@ RSpec.describe User, type: :model do
     user.valid?
     expect(user.errors[:role]).to include("can't be blank")
   end
+
+  it 'is invalid with duplicate email' do
+    user = User.create(
+      name: "Naufal Fadhil Athallah",
+      email: "NaufalFadhil@google.com",
+      phone: "08123456789",
+      password: "adfghjklmnbvcxz",
+      is_verified: true,
+      role: "ADMIN"
+    )
+
+    user2 = User.new(
+      name: "Naufal Fadhil Athallah",
+      email: "NaufalFadhil@google.com",
+      phone: "08123456789",
+      password: "adfghjklmnbvcxz",
+      is_verified: true,
+      role: "ADMIN"
+    )
+
+    user2.valid?
+    expect(user2.errors[:email]).to include("has already been taken")
+  end
+
+  it 'is invalid with duplicate phone number' do
+    user = User.create(
+      name: "Naufal Fadhil Athallah",
+      email: "NaufalFadhil@google.com",
+      phone: "08123456789",
+      password: "adfghjklmnbvcxz",
+      is_verified: true,
+      role: "ADMIN"
+    )
+
+    user2 = User.new(
+      name: "Naufal Fadhil Athallah",
+      email: "NaufalFadhil@google.com",
+      phone: "08123456789",
+      password: "adfghjklmnbvcxz",
+      is_verified: true,
+      role: "ADMIN"
+    )
+
+    user2.valid?
+    expect(user2.errors[:email]).to include("has already been taken")
+  end
 end
