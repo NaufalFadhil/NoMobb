5fc19db0e38f9d88c74bf6eacc81dacb8bd788b7
Naufal Fadhil Athallah <naufalfadhilathallah@gmail.com>
2022-06-30 12:45:23 +0700

    refactory: refactor user authentication

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,32 +1,45 @@
 class Api::UsersController < ApplicationController
+  before_action :authenticate_user
 
-  $masterApiKey = "APIKEY-MASTER@NoMobb.Gigih"
+  # $masterApiKey = "APIKEY-MASTER@NoMobb.Gigih"
 
   def index
-    if params[:api_key] == $masterApiKey
-      @users = User.all
-      render json: {status: true, data: @users}, status: 200
-    else
-      render json: {status: false, error: "Unauthorized"}, status: 401
-    end
+    @users = User.all
+    render json: {status: true, data: @users}, status: 200
   end
 
   def show
-    if params[:api_key] == $masterApiKey
-      @user = User.find_by(id: params[:id])
-      if @user 
-        render json: {status: true, data: @user}, status: 200
-      else
-        render json: {status: false, message: "User not found"}, status: 404
-      end
+    @user = User.find_by(id: params[:id])
+    if @user 
+      render json: {status: true, data: @user}, status: 200
     else
-      render json: {status: false, error: "Unauthorized"}, status: 401
+      render json: {status: false, message: "User not found"}, status: 404
     end
   end
 
   def create
-    if params[:api_key] == $masterApiKey
-      @user = User.create(
+    @user = User.create(
+      name: params[:name],
+      email: params[:email],
+      phone: params[:phone],
+      password: params[:password],
+      is_verified: params[:is_verified],
+      role: params[:role]
+    )
+    
+    begin
+      @user.save
+      render json: {status: true, message: "User has been created", data: @user}, status: 201
+    rescue => exception
+      render json: {status: false, message: "Fail create user", error: exception}, status: 400 
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    begin
+      @user.update(
         name: params[:name],
         email: params[:email],
         phone: params[:phone],
@@ -34,53 +47,21 @@ class Api::UsersController < ApplicationController
         is_verified: params[:is_verified],
         role: params[:role]
       )
-      
-      begin
-        @user.save
-        render json: {status: true, message: "User has been created", data: @user}, status: 201
-      rescue => exception
-        render json: {status: false, message: "Fail create user", error: exception}, status: 400 
-      end
-    else
-      render json: {status: false, error: "Unauthorized"}, status: 401
-    end
-  end
 
-  def update
-    if params[:api_key] == $masterApiKey
-      @user = User.find(params[:id])
-
-      begin
-        @user.update(
-          name: params[:name],
-          email: params[:email],
-          phone: params[:phone],
-          password: params[:password],
-          is_verified: params[:is_verified],
-          role: params[:role]
-        )
-
-        render json: {status: true, message: "User has been edited", data: @user}, status: 201
-      rescue => exception
-        render json: {status: false, message: "Fail update user", error: exception}, status: 400 
-      end
-    else
-      render json: {status: false, error: "Unauthorized"}, status: 401
+      render json: {status: true, message: "User has been edited", data: @user}, status: 201
+    rescue => exception
+      render json: {status: false, message: "Fail update user", error: exception}, status: 400 
     end
   end
 
   def destroy
-    if params[:api_key] == $masterApiKey
-      @user = User.find_by(id: params[:id])
+    @user = User.find_by(id: params[:id])
 
-      begin
-        @user.destroy
-        render json: {status: true, message: "User has been deleted"}, status: 200
-      rescue => exception
-        render json: {status: false, message: "Fail delete user", error: exception}, status: 400 
-      end
-    else
-      render json: {status: false, error: "Unauthorized"}, status: 401
+    begin
+      @user.destroy
+      render json: {status: true, message: "User has been deleted"}, status: 200
+    rescue => exception
+      render json: {status: false, message: "Fail delete user", error: exception}, status: 400 
     end
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :null_session
+
+    $masterApiKey = "APIKEY-MASTER@NoMobb.Gigih"
+
+    def authenticate_user
+        if params[:api_key] != $masterApiKey
+            render json: {status: false, error: "Unauthorized"}, status: 401
+        end
+    end
 end
