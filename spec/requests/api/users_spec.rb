93842f18590a73fad3ced3c2f254722cddc311d4
Naufal Fadhil Athallah <naufalfadhilathallah@gmail.com>
2022-06-30 12:23:24 +0700

    refactory: remove unused testing

--- spec/requests/api/users_spec.rb
+++ spec/requests/api/users_spec.rb
@@ -1,39 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe "Api::Users", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/api/users/index"
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET /index" do
+  #   it "returns http success" do
+  #     get "/api/users/index"
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
-  describe "GET /show" do
-    it "returns http success" do
-      get "/api/users/show"
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET /show" do
+  #   it "returns http success" do
+  #     get "/api/users/show"
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
-  describe "GET /create" do
-    it "returns http success" do
-      get "/api/users/create"
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET /create" do
+  #   it "returns http success" do
+  #     get "/api/users/create"
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
-  describe "GET /update" do
-    it "returns http success" do
-      get "/api/users/update"
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET /update" do
+  #   it "returns http success" do
+  #     get "/api/users/update"
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
-  describe "GET /destroy" do
-    it "returns http success" do
-      get "/api/users/destroy"
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET /destroy" do
+  #   it "returns http success" do
+  #     get "/api/users/destroy"
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
 end
