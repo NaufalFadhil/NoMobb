339c7213c7a9873962768f6df91a1af418b04601
Naufal Fadhil Athallah <naufalfadhilathallah@gmail.com>
2022-07-01 23:30:02 +0700

    fix: create handling if input non numeric phone number

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
     has_secure_password
-    validates :name, presence: true
+    validates :name, :email, presence: true
     validates :is_verified, inclusion: { in: [true, false] }
     validates :role, presence: true, inclusion: { in: ["ADMIN", "BASIC", "PSIKOLOGIST", "INSTITUTION"] }
-    validates :email, :phone, presence: true
+    validates :phone, presence: true, numericality: true
     validates :email, :phone, uniqueness: { case_sensitive: false }, on: :create
 end
