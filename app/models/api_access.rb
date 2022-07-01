e271a899f03e1799e90920d2b1f9f72418c7cdcd
Naufal Fadhil Athallah <naufalfadhilathallah@gmail.com>
2022-07-01 23:56:21 +0700

    refactory: remove generate token in model

--- app/models/api_access.rb
+++ app/models/api_access.rb
@@ -4,13 +4,6 @@ class ApiAccess < ApplicationRecord
     validates :token, uniqueness: true, presence: true
     validates :expired_date, presence: true
 
-    def generate_token
-        self.token = loop do
-            random_token = SecureRandom.urlsafe_base64(nil, false)
-            break random_token unless ApiAccess.exists?(token: random_token)
-        end
-    end
-
     def expired_date_cannot_be_in_the_past
         if expired_date.present? && expired_date < Date.today
             errors.add(:expired_date, "can't be in the past")
