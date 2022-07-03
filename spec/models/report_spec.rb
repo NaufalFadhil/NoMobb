7754969631125b0dcc9b5c9c0f6b9a9315a679ef
Naufal Fadhil Athallah <naufalfadhilathallah@gmail.com>
2022-07-03 14:57:47 +0700

    test(report): create test for API Report Features

--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -1,88 +1,99 @@
 require 'rails_helper'
 
 RSpec.describe Report, type: :model do
-  describe "Field Report Test" do
-    it 'is valid with full input field' do
-      report = Report.new(
-        user_id: 1,
-        perpetrator: 'Naufal Fadhil Athallah',
-        reason: 'Memukul saya',
-        proof: 'https:drive.google.com',
-        witness: 'Nurma',
-        incident_location: 'Sekolah saya',
-        status: 'PENDING',
-        is_valid: false,
-        deleted_at: nil,
-        created_at: 'Sun, 26 Jun 2022 20:43:18.365332000 WIB +07:00',
-        updated_at: 'Sun, 26 Jun 2022 20:43:18.365332000 WIB +07:00',
-      )
+  it 'is valid with full input field' do
+    user = FactoryBot.create(:user)
+    report = FactoryBot.build(:report)
 
-      expect(report).to be_valid
-    end
+    report.valid?
+    expect(report).to be_valid
+  end
 
-    it 'is invalid without perpetrator field' do
-      report = Report.new(
-        perpetrator: nil,
-        reason: nil,
-        proof: nil,
-        witness: nil,
-        incident_location: nil,
-      )
+  it 'is invalid without user_id field' do
+    user = FactoryBot.create(:user)
+    report = FactoryBot.build(:report, user_id: nil)
+    
+    report.valid?
+    expect(report).to be_invalid
+  end
+
+  it 'is invalid without perpetrator fullname field' do
+    user = FactoryBot.create(:user)
+    report = FactoryBot.build(:report, perpetrator_fullname: nil)
+    
+    report.valid?
+    expect(report.errors[:perpetrator_fullname]).to include("can't be blank")
+  end
+
+  it 'is invalid without perpetrator nickname field' do
+    user = FactoryBot.create(:user)
+    report = FactoryBot.build(:report, perpetrator_nickname: nil)
+    
+    report.valid?
+    expect(report.errors[:perpetrator_nickname]).to include("can't be blank")
+  end
+
+  it 'is valid without perpetrator birthday field' do
+    user = FactoryBot.create(:user)
+    report = FactoryBot.build(:report, perpetrator_birthday: nil)
+
+    report.valid?
+    expect(report).to be_valid
+  end
   
-      report.valid?
-      expect(report.errors[:perpetrator]).to include("can't be blank")
-    end
-
-    it 'is invalid without reason field' do
-      report = Report.new(
-        perpetrator: nil,
-        reason: nil,
-        proof: nil,
-        witness: nil,
-        incident_location: nil,
-      )
+  it 'is invalid without proof field' do
+    user = FactoryBot.create(:user)
+    report = FactoryBot.build(:report, proof: nil)
+    
+    report.valid?
+    expect(report).to be_invalid
+  end
   
-      report.valid?
-      expect(report.errors[:reason]).to include("can't be blank")
-    end
+  it 'is invalid without url in proof field' do
+    user = FactoryBot.create(:user)
+    report = FactoryBot.build(:report, proof: "gambar.png")
+    
+    report.valid?
+    expect(report).to be_invalid
+  end
 
-    it 'is invalid without proof field' do
-      report = Report.new(
-        perpetrator: nil,
-        reason: nil,
-        proof: nil,
-        witness: nil,
-        incident_location: nil,
-      )
-  
-      report.valid?
-      expect(report.errors[:proof]).to include("can't be blank")
-    end
+  it 'is invalid without witness field' do
+    user = FactoryBot.create(:user)
+    report = FactoryBot.build(:report, witness: nil)
+    
+    report.valid?
+    expect(report.errors[:witness]).to include("can't be blank")
+  end
 
-    it 'is invalid without witness field' do
-      report = Report.new(
-        perpetrator: nil,
-        reason: nil,
-        proof: nil,
-        witness: nil,
-        incident_location: nil,
-      )
-  
-      report.valid?
-      expect(report.errors[:witness]).to include("can't be blank")
-    end
+  it 'is invalid without nickname field' do
+    user = FactoryBot.create(:user)
+    report = FactoryBot.build(:report, incident_date: nil)
+    
+    report.valid?
+    expect(report.errors[:incident_date]).to include("can't be blank")
+  end
 
-    it 'is invalid without incident location field' do
-      report = Report.new(
-        perpetrator: nil,
-        reason: nil,
-        proof: nil,
-        witness: nil,
-        incident_location: nil,
-      )
-  
-      report.valid?
-      expect(report.errors[:incident_location]).to include("can't be blank")
-    end
+  it 'is invalid without nickname field' do
+    user = FactoryBot.create(:user)
+    report = FactoryBot.build(:report, incident_location: nil)
+    
+    report.valid?
+    expect(report.errors[:incident_location]).to include("can't be blank")
+  end
+
+  it 'is invalid with input birtday in future' do
+    user = FactoryBot.create(:user)
+    report = FactoryBot.build(:report, perpetrator_birthday: "2030-01-01")
+
+    report.valid?
+    expect(report.errors[:perpetrator_birthday]).to include("can't be in the future")
+  end
+
+  it 'is invalid with input incident date in future' do
+    user = FactoryBot.create(:user)
+    report = FactoryBot.build(:report, incident_date: "2030-01-01")
+
+    report.valid?
+    expect(report.errors[:incident_date]).to include("can't be in the future")
   end
 end
