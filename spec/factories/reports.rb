5e2f4d90d04895c17e898cde2378cb7690ae7a4f
Naufal Fadhil Athallah <naufalfadhilathallah@gmail.com>
2022-07-03 14:57:27 +0700

    test(report): add factories for report & user

--- spec/factories/reports.rb
+++ spec/factories/reports.rb
@@ -1,14 +1,15 @@
 FactoryBot.define do
   factory :report do
-    user_id { "MyString" }
-    perpetrator { "MyString" }
-    reason { "MyString" }
-    proof { "MyString" }
-    witness { "MyString" }
-    incident_location { "MyString" }
-    incident_date { "MyString" }
-    status { "MyString" }
-    is_valid { "MyString" }
-    deleted_at { "MyString" }
+    user_id { 1 }
+    perpetrator_fullname { "Naufal Fadhil Athallah" }
+    perpetrator_nickname { "Fadhil" }
+    perpetrator_birthday { "2001-01-01" }
+    reason { "Dibully oleh teman" }
+    proof { "https://drive.google.com" }
+    witness { "Teman" }
+    incident_location { "SMK NEGER 1" }
+    incident_date { "2022-05-01" }
+    status { "PENDING" }
+    is_valid { false }
   end
 end
