8e8e548607323d5900133b47b247539653bc3bd6
Naufal Fadhil Athallah <naufalfadhilathallah@gmail.com>
2022-07-03 17:19:10 +0700

    test: remove unused test for Consultation Message

--- spec/requests/api/consultation_message_spec.rb
+++ spec/requests/api/consultation_message_spec.rb
@@ -1,39 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe "Api::ConsultationMessages", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/api/consultation_message/index"
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET /index" do
+  #   it "returns http success" do
+  #     get "/api/consultation_message/index"
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
-  describe "GET /show" do
-    it "returns http success" do
-      get "/api/consultation_message/show"
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET /show" do
+  #   it "returns http success" do
+  #     get "/api/consultation_message/show"
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
-  describe "GET /create" do
-    it "returns http success" do
-      get "/api/consultation_message/create"
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET /create" do
+  #   it "returns http success" do
+  #     get "/api/consultation_message/create"
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
-  describe "GET /update" do
-    it "returns http success" do
-      get "/api/consultation_message/update"
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET /update" do
+  #   it "returns http success" do
+  #     get "/api/consultation_message/update"
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
-  describe "GET /destroy" do
-    it "returns http success" do
-      get "/api/consultation_message/destroy"
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET /destroy" do
+  #   it "returns http success" do
+  #     get "/api/consultation_message/destroy"
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
 end
